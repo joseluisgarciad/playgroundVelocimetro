2256c7ccfdb2e980e262f2c340591463b7a57e9e
Jose Luis Garcia Dueñas <joseluisgarciad@gmail.com>
2016-01-31 11:32:22 +0100

    1

---
+++ PlaygroundVelocimetro/PlaygroundVelocimetro.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+enum Velocidades :Int {
+    case Apagado = 0
+    case VelocidadBaja = 20
+    case VelocidadMedia = 50
+    case VelocidadAlta = 120
+}
+//
